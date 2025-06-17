dc624155abb55c54d9a045099d68b7f34bb2ba81
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-06-17 13:03:43 +0530

    versel deploy error fix - 3

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true
   },
+  env: {
+    NEXT_PUBLIC_BASE_API_URL: "https://your-api.vercel.app/api" // or your backend URL
+  }
 };
 
 export default nextConfig;
