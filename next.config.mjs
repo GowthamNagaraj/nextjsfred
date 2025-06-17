0964614faff227a80c2ccff0ec0e09db03bf51c1
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-06-17 12:44:11 +0530

    deploy error fix 2 times

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "out",
-    reactStrictMode: true,
-    eslint: {
-        ignoreDuringBuilds: true, // Ignore ESLint warnings during build
-    },
-    env:{
-        NEXT_BASE_API_URL : "http://localhost:1998/GMND/api"
-    }
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
